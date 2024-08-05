1974449fc53fd20e9556d317955cd9c2712ee7ec
Wyatt Smith <wyattjorgen@gmail.com>
2024-08-04 18:06:38 -0700

    Added functionality to the Cancel button, it now closes the form.

--- StardewGroupProject/Forms/AddNewFarmForm.Designer.cs
+++ StardewGroupProject/Forms/AddNewFarmForm.Designer.cs
@@ -71,6 +71,7 @@
 			btnNewFarmCancel.TabIndex = 3;
 			btnNewFarmCancel.Text = "Cancel";
 			btnNewFarmCancel.UseVisualStyleBackColor = true;
+			btnNewFarmCancel.Click += btnNewFarmCancel_Click;
 			// 
 			// AddNewFarmForm
 			// 
--- StardewGroupProject/Forms/AddNewFarmForm.cs
+++ StardewGroupProject/Forms/AddNewFarmForm.cs
@@ -44,5 +44,10 @@ namespace StardewGroupProject
 				txtNewFarmName.Clear();
 			}
 		}
+
+		private void btnNewFarmCancel_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
 	}
 }
