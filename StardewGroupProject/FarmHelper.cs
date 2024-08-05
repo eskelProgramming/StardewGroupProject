5683bad78b18ee13e58408775aa62b1a1d46f775
Wyatt Smith <wyattjorgen@gmail.com>
2024-08-05 15:07:11 -0700

    Created the FarmHelper class, and implemented the PopulateFarmCmbBox method

---
+++ StardewGroupProject/FarmHelper.cs
@@ -0,0 +1,26 @@
+﻿using StardewGroupProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StardewGroupProject
+{
+	public class FarmHelper
+	{
+		public static void PopulateFarmCmbBox(ComboBox cmbFarmNames)
+		{
+			FarmContext farmContext = new();
+
+			cmbFarmNames.DataSource = null; // Temporarily un-binds the ComboBox from the datasource
+
+			cmbFarmNames.Items.Clear();
+
+			var data = farmContext.Farms.ToList(); // Puts all the farms from the database into a list
+
+			cmbFarmNames.DataSource = data; // Re-binds the ComboBox's datasource to the list of items
+			cmbFarmNames.DisplayMember = "Name"; // Displays the Name property from the data
+		}
+	}
+}
