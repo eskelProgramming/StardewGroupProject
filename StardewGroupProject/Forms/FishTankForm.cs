764af6c07bf3a355fe4a826e5e4174f24cdb96ae
eskelProgramming <eskelley0673@students.cptc.edu>
2024-08-20 15:16:12 -0700

    Added population for FishTank form

--- StardewGroupProject/Forms/FishTankForm.cs
+++ StardewGroupProject/Forms/FishTankForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StardewGroupProject.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,51 @@ using System.Windows.Forms;
 
 namespace StardewGroupProject
 {
-	public partial class FishTankForm : Form
-	{
-		public FishTankForm()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class FishTankForm : Form
+    {
+        public FishTankForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FishTankForm_Load(object sender, EventArgs e)
+        {
+            LoadBundles();
+        }
+
+        private void LoadBundles()
+        {
+            List<Bundle> bundles = ObjectTransferHelper.CurrentFarm.GetRoom("Fish Tank").Bundles;
+
+            foreach(Item item in bundles[0].Items)
+            {
+                cklRiverFish.Items.Add(item.Name);
+            }
+
+            foreach(Item item in bundles[1].Items)
+            {
+                cklLakeFish.Items.Add(item.Name);
+            }
+
+            foreach(Item item in bundles[2].Items)
+            {
+                cklOceanFish.Items.Add(item.Name);
+            }
+
+            foreach(Item item in bundles[3].Items)
+            {
+                cklNightFishing.Items.Add(item.Name);
+            }
+
+            foreach(Item item in bundles[4].Items)
+            {
+                cklCrabPot.Items.Add(item.Name);
+            }
+
+            foreach(Item item in bundles[5].Items)
+            {
+                cklSpecialtyFish.Items.Add(item.Name);
+            }
+        }
+    }
 }
