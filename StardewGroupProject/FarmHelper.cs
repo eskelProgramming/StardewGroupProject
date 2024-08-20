c599aa60717deec475adede8b3ac1602294bb356
Wyatt Smith <wyattjorgen@gmail.com>
2024-08-19 19:30:46 -0700

    My progress on working on the delete button

--- StardewGroupProject/FarmHelper.cs
+++ StardewGroupProject/FarmHelper.cs
@@ -1,4 +1,6 @@
-﻿using StardewGroupProject.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using StardewGroupProject.Data;
+using StardewGroupProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,5 +24,48 @@ namespace StardewGroupProject
 			cmbFarmNames.DataSource = data; // Re-binds the ComboBox's datasource to the list of items
 			cmbFarmNames.DisplayMember = "Name"; // Displays the Name property from the data
 		}
-	}
+
+		public static void DeleteFarm(Models.Farm farm)
+		{
+			FarmContext farmContext = new();
+
+            var roomsToDelete = farmContext.Rooms
+                .Where(room => EF.Property<int>(room, "FarmId") == farm.FarmId)
+                .ToList();
+
+            foreach (var room in roomsToDelete)
+            {
+                // Load the bundles associated with the room
+                var bundlesToDelete = farmContext.Bundles
+                    .Where(bundle => EF.Property<int>(bundle, "RoomId") == room.RoomId)
+                    .ToList();
+
+                foreach (var bundle in bundlesToDelete)
+                {
+                    // Load the items associated with the bundle
+                    var itemsToDelete = farmContext.Items
+                        .Where(item => EF.Property<int>(item, "BundleId") == bundle.BundleId)
+                        .ToList();
+
+                    // Delete each item
+                    foreach (var item in itemsToDelete)
+                    {
+                        farmContext.Items.Remove(item);
+                    }
+
+                    // Delete the bundle
+                    farmContext.Bundles.Remove(bundle);
+                }
+
+                // Delete the room
+                farmContext.Rooms.Remove(room);
+            }
+
+            // Delete the farm itself
+            farmContext.Farms.Remove(farm);
+
+            // Save changes to commit deletions
+            farmContext.SaveChanges();
+        }
+    }
 }
--- StardewGroupProject/Forms/HomeForm.cs
+++ StardewGroupProject/Forms/HomeForm.cs
@@ -1,36 +1,45 @@
+using Microsoft.EntityFrameworkCore;
 using StardewGroupProject.Data;
 using StardewGroupProject.Models;
+using System.Windows.Forms;
 
 namespace StardewGroupProject
 {
-	public partial class HomeForm : Form
-	{
-		public HomeForm()
-		{
-			InitializeComponent();
-		}
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+        }
 
-		private void btnAddNewFarm_Click(object sender, EventArgs e)
-		{
-			AddNewFarmForm addNewFarm = new();
-			addNewFarm.ShowDialog();
+        private void btnAddNewFarm_Click(object sender, EventArgs e)
+        {
+            AddNewFarmForm addNewFarm = new();
+            addNewFarm.ShowDialog();
 
-			FarmHelper.PopulateFarmCmbBox(cmbFarmNames);
-		}
+            FarmHelper.PopulateFarmCmbBox(cmbFarmNames);
+        }
 
-		private void btnViewCommunityCenter_Click(object sender, EventArgs e)
-		{
-			BundlesForm bundles = new BundlesForm();
-			this.Hide();
-			bundles.FormClosed += (s, args) => this.Show(); // Shows the current form (Home) when the Bundles form is closed
-			bundles.ShowDialog();
-		}
+        private void btnViewCommunityCenter_Click(object sender, EventArgs e)
+        {
+            BundlesForm bundles = new BundlesForm();
+            this.Hide();
+            bundles.FormClosed += (s, args) => this.Show(); // Shows the current form (Home) when the Bundles form is closed
+            bundles.ShowDialog();
+        }
 
-		private void HomeForm_Load(object sender, EventArgs e)
-		{
-			FarmHelper.PopulateFarmCmbBox(cmbFarmNames);
-		}
+        private void HomeForm_Load(object sender, EventArgs e)
+        {
+            FarmHelper.PopulateFarmCmbBox(cmbFarmNames);
+        }
 
+        private void btnDeleteFarm_Click(object sender, EventArgs e)
+        {
+            var selectedFarm = cmbFarmNames.SelectedItem as Farm;
 
-	}
+            FarmHelper.DeleteFarm(selectedFarm);
+
+            FarmHelper.PopulateFarmCmbBox(cmbFarmNames);
+        }
+    }
 }
