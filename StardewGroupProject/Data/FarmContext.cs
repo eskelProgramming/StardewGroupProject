dd158ac3ea73bd5f59f332f14616c60aeefa3186
Wyatt Smith <wyattjorgen@gmail.com>
2024-08-19 19:46:33 -0700

    Fixed FarmContext to allow cascading behaviors. The Delete button should now be fully functional

--- StardewGroupProject/Data/FarmContext.cs
+++ StardewGroupProject/Data/FarmContext.cs
@@ -14,6 +14,30 @@ namespace StardewGroupProject.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Farm>()
+                .HasMany(f => f.Rooms)
+                .WithOne()
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+            // Room has many Bundles, when a Room is deleted, Bundles should also be deleted.
+            modelBuilder
+                .Entity<Room>()
+                .HasMany(r => r.Bundles)
+                .WithOne()
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+            // Bundle has many Items, when a Bundle is deleted, Items should also be deleted.
+            modelBuilder
+                .Entity<Bundle>()
+                .HasMany(b => b.Items)
+                .WithOne()
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=StardewGroupProject;Trusted_Connection=True;");
