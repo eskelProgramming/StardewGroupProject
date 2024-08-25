b7517385162782cc380515f9abaa6f61cb8b4829
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-08-24 17:06:14 -0800

    Update now works on the FishTank form

--- StardewGroupProject/Forms/FishTankForm.Designer.cs
+++ StardewGroupProject/Forms/FishTankForm.Designer.cs
@@ -46,9 +46,9 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Sitka Banner", 12F);
-            label6.Location = new Point(409, 242);
+            label6.Location = new Point(467, 323);
             label6.Name = "label6";
-            label6.Size = new Size(78, 23);
+            label6.Size = new Size(97, 29);
             label6.TabIndex = 23;
             label6.Text = "Ocean Fish";
             // 
@@ -56,9 +56,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Sitka Banner", 12F);
-            label5.Location = new Point(419, 59);
+            label5.Location = new Point(479, 79);
             label5.Name = "label5";
-            label5.Size = new Size(96, 23);
+            label5.Size = new Size(120, 29);
             label5.TabIndex = 22;
             label5.Text = "Specialty Fish";
             // 
@@ -66,9 +66,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Sitka Banner", 12F);
-            label4.Location = new Point(86, 245);
+            label4.Location = new Point(98, 327);
             label4.Name = "label4";
-            label4.Size = new Size(69, 23);
+            label4.Size = new Size(86, 29);
             label4.TabIndex = 21;
             label4.Text = "Lake Fish";
             // 
@@ -76,9 +76,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Sitka Banner", 12F);
-            label3.Location = new Point(254, 166);
+            label3.Location = new Point(290, 221);
             label3.Name = "label3";
-            label3.Size = new Size(64, 23);
+            label3.Size = new Size(79, 29);
             label3.TabIndex = 20;
             label3.Text = "Crab Pot";
             // 
@@ -86,9 +86,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Sitka Banner", 12F);
-            label2.Location = new Point(44, 59);
+            label2.Location = new Point(50, 79);
             label2.Name = "label2";
-            label2.Size = new Size(93, 23);
+            label2.Size = new Size(117, 29);
             label2.TabIndex = 19;
             label2.Text = "Night Fishing";
             // 
@@ -96,9 +96,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Sitka Banner", 12F);
-            label1.Location = new Point(245, 17);
+            label1.Location = new Point(280, 23);
             label1.Name = "label1";
-            label1.Size = new Size(73, 23);
+            label1.Size = new Size(90, 29);
             label1.TabIndex = 18;
             label1.Text = "River Fish";
             // 
@@ -106,61 +106,73 @@
             // 
             cklOceanFish.Font = new Font("Sitka Banner", 12F);
             cklOceanFish.FormattingEnabled = true;
-            cklOceanFish.Location = new Point(388, 271);
+            cklOceanFish.Location = new Point(443, 361);
+            cklOceanFish.Margin = new Padding(3, 4, 3, 4);
             cklOceanFish.Name = "cklOceanFish";
-            cklOceanFish.Size = new Size(120, 73);
+            cklOceanFish.Size = new Size(137, 88);
             cklOceanFish.TabIndex = 17;
+            cklOceanFish.ItemCheck += cklOceanFish_ItemCheck;
             // 
             // cklSpecialtyFish
             // 
             cklSpecialtyFish.Font = new Font("Sitka Banner", 12F);
             cklSpecialtyFish.FormattingEnabled = true;
-            cklSpecialtyFish.Location = new Point(409, 85);
+            cklSpecialtyFish.Location = new Point(467, 113);
+            cklSpecialtyFish.Margin = new Padding(3, 4, 3, 4);
             cklSpecialtyFish.Name = "cklSpecialtyFish";
-            cklSpecialtyFish.Size = new Size(120, 73);
+            cklSpecialtyFish.Size = new Size(137, 88);
             cklSpecialtyFish.TabIndex = 16;
+            cklSpecialtyFish.ItemCheck += cklSpecialtyFish_ItemCheck;
             // 
             // cklLakeFish
             // 
             cklLakeFish.Font = new Font("Sitka Banner", 12F);
             cklLakeFish.FormattingEnabled = true;
-            cklLakeFish.Location = new Point(67, 271);
+            cklLakeFish.Location = new Point(77, 361);
+            cklLakeFish.Margin = new Padding(3, 4, 3, 4);
             cklLakeFish.Name = "cklLakeFish";
-            cklLakeFish.Size = new Size(120, 73);
+            cklLakeFish.Size = new Size(137, 88);
             cklLakeFish.TabIndex = 15;
+            cklLakeFish.ItemCheck += cklLakeFish_ItemCheck;
             // 
             // cklCrabPot
             // 
             cklCrabPot.Font = new Font("Sitka Banner", 12F);
             cklCrabPot.FormattingEnabled = true;
-            cklCrabPot.Location = new Point(225, 192);
+            cklCrabPot.Location = new Point(257, 256);
+            cklCrabPot.Margin = new Padding(3, 4, 3, 4);
             cklCrabPot.Name = "cklCrabPot";
-            cklCrabPot.Size = new Size(120, 73);
+            cklCrabPot.Size = new Size(137, 88);
             cklCrabPot.TabIndex = 14;
+            cklCrabPot.ItemCheck += cklCrabPot_ItemCheck;
             // 
             // cklNightFishing
             // 
             cklNightFishing.Font = new Font("Sitka Banner", 12F);
             cklNightFishing.FormattingEnabled = true;
-            cklNightFishing.Location = new Point(35, 85);
+            cklNightFishing.Location = new Point(40, 113);
+            cklNightFishing.Margin = new Padding(3, 4, 3, 4);
             cklNightFishing.Name = "cklNightFishing";
-            cklNightFishing.Size = new Size(120, 73);
+            cklNightFishing.Size = new Size(137, 88);
             cklNightFishing.TabIndex = 13;
+            cklNightFishing.ItemCheck += cklNightFishing_ItemCheck;
             // 
             // cklRiverFish
             // 
             cklRiverFish.Font = new Font("Sitka Banner", 12F);
             cklRiverFish.FormattingEnabled = true;
-            cklRiverFish.Location = new Point(225, 43);
+            cklRiverFish.Location = new Point(257, 57);
+            cklRiverFish.Margin = new Padding(3, 4, 3, 4);
             cklRiverFish.Name = "cklRiverFish";
-            cklRiverFish.Size = new Size(120, 73);
+            cklRiverFish.Size = new Size(137, 88);
             cklRiverFish.TabIndex = 12;
+            cklRiverFish.ItemCheck += cklRiverFish_ItemCheck;
             // 
             // FishTankForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(568, 405);
+            ClientSize = new Size(649, 540);
             Controls.Add(label6);
             Controls.Add(label5);
             Controls.Add(label4);
@@ -173,6 +185,7 @@
             Controls.Add(cklCrabPot);
             Controls.Add(cklNightFishing);
             Controls.Add(cklRiverFish);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FishTankForm";
             Text = "Fish Tank Bundles";
             Load += FishTankForm_Load;
