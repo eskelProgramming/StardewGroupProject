1dd7a6afe193d46cef231fbee3aac976abb27425
Wyatt Smith <wyattjorgen@gmail.com>
2024-09-09 10:18:49 -0700

    Refactored DeleteFarm method to clear up unnecessary code

--- StardewGroupProject/FarmHelper.cs
+++ StardewGroupProject/FarmHelper.cs
@@ -25,38 +25,6 @@ namespace StardewGroupProject
 
 		public static void DeleteFarm(Models.Farm farm)
 		{
-            var roomsToDelete = ObjectTransferHelper.Context.Rooms
-                .Where(room => EF.Property<int>(room, "FarmId") == farm.FarmId)
-                .ToList();
-
-            foreach (var room in roomsToDelete)
-            {
-                // Load the bundles associated with the room
-                var bundlesToDelete = ObjectTransferHelper.Context.Bundles
-                    .Where(bundle => EF.Property<int>(bundle, "RoomId") == room.RoomId)
-                    .ToList();
-
-                foreach (var bundle in bundlesToDelete)
-                {
-                    // Load the items associated with the bundle
-                    var itemsToDelete = ObjectTransferHelper.Context.Items
-                        .Where(item => EF.Property<int>(item, "BundleId") == bundle.BundleId)
-                        .ToList();
-
-                    // Delete each item
-                    foreach (var item in itemsToDelete)
-                    {
-                        ObjectTransferHelper.Context.Items.Remove(item);
-                    }
-
-                    // Delete the bundle
-                    ObjectTransferHelper.Context.Bundles.Remove(bundle);
-                }
-
-                // Delete the room
-                ObjectTransferHelper.Context.Rooms.Remove(room);
-            }
-
             // Delete the farm itself
             ObjectTransferHelper.Context.Farms.Remove(farm);
 
